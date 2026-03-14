160ac8d39ec16f81b94c3aaee6795f8f91ae7473
Minakshi-Naukarkar <minakshinaukarkarwork@gmail.com>
2026-03-14 15:19:01 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,4 +1,2 @@
 pytest -v -s -n=2 --html=Html_Reports\OrangeHRM_Login_chrome.html --alluredir=Allure_Reports --browser chrome
 pytest -v -s -n=2 --html=Html_Reports\OrangeHRM_Login_firefox.html --alluredir=Allure_Reports --browser firefox
-pytest -v -s -n=2 --html=Html_Reports\OrangeHRM_Login_edge.html --alluredir=Allure_Reports --browser edge
-pytest -v -s -n=2 --html=Html_Reports\OrangeHRM_Login_headless.html --alluredir=Allure_Reports --browser headless
